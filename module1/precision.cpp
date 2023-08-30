714a045ffaadc7548b1f5dab7ef03e0cebf1e0c3
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-08-30 23:07:27 +0600

    basic input output practiced again and some build in function used

---
+++ module1/precision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    /*code*/
+    float n;
+    cin >> n;
+    cout << fixed << setprecision(3) << n;
+
+    return 0;
+}
